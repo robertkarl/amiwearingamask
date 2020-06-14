d699613eb4a1678b7244606fe3dac6507050ccf0
Robert Karl <robertkarljr@gmail.com>
2020-06-14 13:30:48 -0700

    Check if you are wearing a mask from your browser

--- src/amiwearingamask/webapp.py
+++ src/amiwearingamask/webapp.py
@@ -1,8 +1,28 @@
 import flask
+
 import fastai
+from fastai.vision import load_learner
+
 
 bp = flask.Blueprint("webapp", __name__)
+model = load_learner('model')
 
-@bp.route('/asdf')
-def asdf():
-    return flask.render_template('asdf.html')
+classes = ['mask', 'nomask']
+def get_prediction_name(image_name):
+    image = fastai.vision.open_image(image_name)
+    val = model.predict(image)
+    argmaxres = val[1].argmax()
+    highest_category = argmaxres.item()
+    res = classes[highest_category]
+    return res
+
+
+@bp.route('/', methods=["GET", "POST"])
+def predict():
+    val = None
+    if flask.request.method == 'POST':
+        img_name = 'tmp.png'
+        flask.request.files['input_image'].save('tmp.png')
+        val = get_prediction_name(img_name)
+    return flask.render_template('masked.html', result=val)
+
